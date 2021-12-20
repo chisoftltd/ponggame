001f083ad0c2559e81a890da7361e7cd631c3176
chisoftltd <chisoftltd@gmail.com>
2021-12-20 18:46:19 +0000

    Creating and get the Paddles to move

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 
 def setup_board(pong_list):
@@ -6,45 +7,9 @@ def setup_board(pong_list):
         pong = Turtle("square")
         pong.penup()
         pong.color("white")
-        pong.shapesize(stretch_wid=, stretch_len=.5)
+        pong.shapesize(stretch_wid=1, stretch_len=.5)
         pong.goto(0, position)
         pong_list.append(pong)
-
-
-def right_bar(pong_list):
-    print(pong_list[0].ycor())
-    if pong_list[0].ycor() >= 200:
-        start = 300
-        end = -300
-        step = -20
-    elif pong_list[0].ycor() <= -200:
-        start = -300
-        end = 300
-        step = 20
-    for right_bar_position in range(start, end, step):
-        print(f"right_bar_position {right_bar_position}")
-        pong_list[0].penup()
-        pong_list[0].goto(400, right_bar_position)
-        pong_list[0].clear()
-        pong_list[0].shapesize(1, 0.25)
-
-
-def left_bar(pong_list):
-    print(pong_list[1].ycor())
-    if pong_list[1].ycor() >= 200:
-        start = -300
-        end = 300
-        step = 20
-    elif pong_list[1].ycor() <= -200:
-        start = 300
-        end = -300
-        step = -20
-    for left_bar_position in range(start, end, step):
-        print(f"left_bar_position {left_bar_position}")
-        pong_list[1].penup()
-        pong_list[1].goto(-400, left_bar_position)
-        pong_list[1].clear()
-        pong_list[1].shapesize(1, 0.25)
 
 
 class Board(Turtle):
@@ -53,11 +18,19 @@ class Board(Turtle):
         super().__init__()
         self.pong_list = []
         setup_board(self.pong_list)
-        right_bar(self.pong_list)
-        left_bar(self.pong_list)
+        self.bar_right = self.pong_list[0].clone()
+        self.bar_left = self.pong_list[0].clone()
+        self.bar_right.goto(400, 0)
+        self.bar_left.goto(-400, 0)
 
-    def go_up(self):
-        self.pong_list[0].goto(self.pong_list[0].xcor(), self.pong_list[0].xcor() + 20)
+    def go_up_right(self):
+        self.bar_right.goto(self.bar_right.xcor(), (self.bar_right.ycor() + random.randint(5, 10)))
 
-    def go_down(self):
-        self.pong_list[0].goto(self.pong_list[0].xcor(), self.pong_list[0].xcor() - 20)
+    def go_down_right(self):
+        self.bar_right.goto(self.bar_right.xcor(), (self.bar_right.ycor() - random.randint(5, 10)))
+
+    def go_up_left(self):
+        self.bar_left.goto(self.bar_left.xcor(), (self.bar_left.ycor() + random.randint(5, 10)))
+
+    def go_down_left(self):
+        self.bar_left.goto(self.bar_left.xcor(), (self.bar_left.ycor() - random.randint(5, 10)))
--- main.py
+++ main.py
@@ -7,16 +7,21 @@ screen.setup(900, 700)
 screen.bgcolor("black")
 screen.title("Pong")
 score = Score()
-screen.tracer()
+screen.tracer(0)
 
 board = Board()
 screen.listen()
-screen.onkey(board.go_up, "Up")
-screen.onkey(board.go_down, "Down")
+# screen.onkey(board.go_up_right, "Up")
+# screen.onkey(board.go_down_right, "Down")
 is_game_on = True
 
 while is_game_on:
-
+    for _ in range(1):
+        board.go_up_right()
+        board.go_down_left()
+    for _ in range(1):
+        board.go_up_left()
+        board.go_down_right()
     screen.update()
 
 
