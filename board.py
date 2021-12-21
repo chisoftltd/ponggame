d19ce98b07e6b7e6d81f07a65f76dc2b01a31d49
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-21 13:17:44 +0000

    Create ball and get it moving and bouncing.

--- board.py
+++ board.py
@@ -1,36 +1,20 @@
 from turtle import Turtle
-import random
 
-
-def setup_board(pong_list):
-    for position in range(-300, 350, 40):
-        pong = Turtle("square")
-        pong.penup()
-        pong.color("white")
-        pong.shapesize(stretch_wid=1, stretch_len=.5)
-        pong.goto(0, position)
-        pong_list.append(pong)
 
 
 class Board(Turtle):
 
-    def __init__(self):
+    def __init__(self, x_position, y_position):
         super().__init__()
-        self.pong_list = []
-        setup_board(self.pong_list)
-        self.bar_right = self.pong_list[0].clone()
-        self.bar_left = self.pong_list[0].clone()
-        self.bar_right.goto(400, 0)
-        self.bar_left.goto(-400, 0)
+        self.penup()
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_wid=2, stretch_len=0.5)
+        self.goto(x_position, y_position)
 
-    def go_up_right(self):
-        self.bar_right.goto(self.bar_right.xcor(), (self.bar_right.ycor() + random.randint(5, 10)))
+    def go_up(self):
+        self.goto(self.xcor(), (self.ycor() + 50))
 
-    def go_down_right(self):
-        self.bar_right.goto(self.bar_right.xcor(), (self.bar_right.ycor() - random.randint(5, 10)))
+    def go_down(self):
+        self.goto(self.xcor(), (self.ycor() - 50))
 
-    def go_up_left(self):
-        self.bar_left.goto(self.bar_left.xcor(), (self.bar_left.ycor() + random.randint(5, 10)))
-
-    def go_down_left(self):
-        self.bar_left.goto(self.bar_left.xcor(), (self.bar_left.ycor() - random.randint(5, 10)))
--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 from turtle import Screen
+import time
+
+from ball import Ball
 from score import Score
 from board import Board
 
 screen = Screen()
-screen.setup(900, 700)
+screen.setup(1050, 700)
 screen.bgcolor("black")
 screen.title("Pong")
 score = Score()
 screen.tracer(0)
 
-board = Board()
+right_board = Board(500, 0)
+left_board = Board(-500, 0)
+ball = Ball()
 screen.listen()
-# screen.onkey(board.go_up_right, "Up")
-# screen.onkey(board.go_down_right, "Down")
+screen.onkey(right_board.go_up, "Up")
+screen.onkey(right_board.go_down, "Down")
+screen.onkey(left_board.go_up, "a")
+screen.onkey(left_board.go_down, "z")
+
+for position in range(-300, 300, 50):
+    center_board = Board(0, position)
+
+
 is_game_on = True
-
 while is_game_on:
-    for _ in range(1):
-        board.go_up_right()
-        board.go_down_left()
-    for _ in range(1):
-        board.go_up_left()
-        board.go_down_right()
+    ball.move()
     screen.update()
-
-
+    time.sleep(0.1)
+    if ball.ycor() > 250 or ball.ycor() < -250:
+        ball.bounce()
 screen.exitonclick()
